b0d8762c987e5b7759cea9cc333155ed22342268
Krzysztof Gwóźdź <krzysztof.gwozdz@outlook.com>
2024-07-01 20:28:51 +0200

    Display list of files

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+const string dataDirectoryPath = @"..\\..\\..\\..\\test_data";
+
+AnsiConsole.Write(new FigletText("C#").Color(Color.Green1));
+
+var files =
+    Directory.GetFiles(dataDirectoryPath, "*.md", SearchOption.TopDirectoryOnly)
+        .Select(file => (
+                Name: Path.GetFileName(file),
+                Path: file
+            )
+        )
+        .ToList();
+
+AnsiConsole.MarkupLine("[bold]Files[/]");
+files.ForEach(file => Console.WriteLine(file.Name));
+PrintSeparator();
+
+return;
+
+void PrintSeparator() =>
+    Console.WriteLine($"{Environment.NewLine}--- --- --- --- ---{Environment.NewLine}");
--- F#/Program.fs
+++ F#/Program.fs
@@ -1,1 +1,19 @@
-﻿printfn "Hello, World!"
+﻿open System
+open System.IO
+open Spectre.Console
+
+let dataDirectoryPath = @"..\\..\\..\\..\\test_data"
+
+let printSeparator () =
+    printfn $"{Environment.NewLine}--- --- --- --- ---{Environment.NewLine}"
+
+AnsiConsole.Write(FigletText("F#"))
+
+let files =
+    Directory.GetFiles(dataDirectoryPath, "*.md", SearchOption.TopDirectoryOnly)
+    |> Array.map (fun file -> (Path.GetFileName file, file))
+
+AnsiConsole.MarkupLine("[bold]Files[/]")
+files |> Array.iter (fun (name, _) -> printfn $"%s{name}")
+printSeparator ()
+
